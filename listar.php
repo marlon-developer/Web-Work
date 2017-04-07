ca64109cb6a34cd0f9e4e53152fddcb6fecefd5d
Márlon <marlonti17@gmail.com>
2017-04-07 20:19:23 -0300

    Trabalho Web
    Adicionado Código para listar arquivos do diretório

--- listar.php
+++ listar.php
@@ -8,12 +8,21 @@
 	}*/
 
 
-	$abrir_arquivo = fopen("kkk_kkk.txt", "r");
+	/*$abrir_arquivo = fopen("*.txt", "r");
 	while (!feof ($abrir_arquivo)) {
 		$linha = fgets($abrir_arquivo, 4096);
 		echo $linha ."<br>";
-	}
+	}*/
 
+	//Listar Arquivos do Diretorio Arquivos
+	$pasta = "arquivos";
+	$listar = new RecursiveDirectoryIterator($pasta);
+	$recursivo = new RecursiveIteratorIterator($listar);
+
+	//Printando o nome dos Arquivos existentes na pasta
+	foreach ($recursivo as $objeto) echo $objeto -> getFilename()."<br>";
+	
+	
 ?>
 
 
